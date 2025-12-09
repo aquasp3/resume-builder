5244d15a35dffd76dd2034a4621dfd9cf89623f5
aquasp3 <sathvikkonduri14@gmail.com>
2025-12-09 17:26:23 +0530

    Delete mobile directory

--- mobile/src/supabaseClient.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-const SUPABASE_URL = "https://qkzjozlswqlxdxlbrkgx.supabase.co";
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFrempvemxzd3FseGR4bGJya2d4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjI3Nzg2NDMsImV4cCI6MjA3ODM1NDY0M30.ew6q6-4mqdpGeg77Zol1iOFGk3XjAjJhuZn76Hz_gU0";
-
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
